1fd7bcf54ffb2f1c8dcf9f8cbbca43e5c4e482c7
muhammad idrees <idreesiqbal104@gmail.com>
2019-06-27 00:06:24 -0700

    clicent section done with responsive

--- js/custom.js
+++ js/custom.js
@@ -49,7 +49,7 @@ $('.slider-carousel').slick({
 
 // digital-carousel
 $('.digital-carousel').slick({
-  dots: false,
+  dots: true,
   prevArrow: false,
   nextArrow: false,
   infinite: false,
@@ -64,7 +64,47 @@ $('.digital-carousel').slick({
         slidesToScroll: 3,
         infinite: true,
         dots: true,
-        arrows: false
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+$('.admin-carousel').slick({
+  dots: true,
+  prevArrow: false,
+  nextArrow: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: false,
       }
     },
     {
